c782d70af0aee1e65a4cd20759ec3d1263759920
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-08-07 15:41:09 +1000

    Additions to Javascript file to fetch API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,63 @@
-const timeEl = document.querySelector("#time");
-const dateEl = document.querySelector("#date");
-const currentWeatherItemsEl = document.querySelector("#current-weather-items");
-const timezone = document.querySelector("#time-zone");
-const countryEl = document.querySelector("#country");
-const weatherForecastEl = document.querySelector("#weather-forecast");
-const currentTempEl = document.querySelector("#current-temp");
-
-
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const userFormEl = $(".search-box");
+const cityInput = $("#search-query");
+const submitBtn = $(".submit");
+const prevContainer = $(".prevsearch-container")
+const prevEl = $(".prevsearch-box");
+const cityEl = $("#city");
+const dateEl = $("#date");
+const currentWeather = $("#current-weather-items");
+const futureWeather = $("#weather-forecast");
+const cityArr = [];
+const currentDate = moment().format("DD/MM/YYYY");
 
 const API_KEY = "d7ddb6b485324f8476383582a2ea3896";
 
-setInterval(() => {
-  const time = new Date();
-  const month = time.getMonth();
-  const date = time.getDate();
-  const day = time.getDay();
-  const hour = time.getHours();
-  const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour
-  const minutes = time.getMinutes();
-  const ampm = hour >= 12 ? 'PM' : 'AM'
+$(document).ready(function () {
+  let pastInput = JSON.parse(localStorage.getItem("city"));
+  console.log(pastInput);
+  if (pastInput !== null) {
+    for (var i = 0; i < pastInput.length; i++) {
+      var liEl = $("<li class='li-el-results'>" + pastInput[i] + "</li>");
+      prevEl.append(liEl);
+      prevContainer.removeClass("d-none");
+      console.log(liEl);
+    };
+  };
+});
 
-  timeEl.innerHTML = hoursIn12HrFormat + ':' + minutes + ' ' + `<span id="am-pm>${ampm}</span>`
+function inputHandler(event) {
+  event.preventDefault();
+  currentWeather.empty();
+  futureWeather.empty();
 
-  dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
+  let location = cityInput.val().trim();
+  console.log(location);
 
-}, 1000);
- /*
-const baseApiUrl = "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}"
-const searchApiUrl = `${baseApiUrl}/Search`
-class requestController {
- getLocation() {
-     $.getJSON(baseApiUrl, 'Sydney').done(data => console.log(data));
- }
+  if (location) {
+    getWeather(location);
+    cityInput.val("");
+  } else {
+    alert("Please enter a location to continue. ");
+  };
+};
+
+function getWeather(location) {
+  let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + API_KEY;
+  let forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&units=metric&appid=" + API_KEY;
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayWeather(data, location);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("City not found. ");
+    });
 }
-
-const request = new getLocation(): void
- request.getLocation(); void
-
-*/
