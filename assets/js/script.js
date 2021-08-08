68150a836cb4f659fb341f86d939e83142177724
Steph Melanofridis <stephmelanofridis@bigpond.com>
2021-08-08 17:24:54 +1000

    Update to code attempting to connect to Open Weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,13 +14,11 @@ const API_KEY = "d7ddb6b485324f8476383582a2ea3896";
 
 $(document).ready(function () {
   let pastInput = JSON.parse(localStorage.getItem("city"));
-  console.log(pastInput);
   if (pastInput !== null) {
     for (var i = 0; i < pastInput.length; i++) {
       var liEl = $("<li class='li-el-results'>" + pastInput[i] + "</li>");
       prevEl.append(liEl);
       prevContainer.removeClass("d-none");
-      console.log(liEl);
     };
   };
 });
@@ -31,7 +29,6 @@ function inputHandler(event) {
   futureWeather.empty();
 
   let location = cityInput.val().trim();
-  console.log(location);
 
   if (location) {
     getWeather(location);
@@ -60,4 +57,157 @@ function getWeather(location) {
     .catch(function (error) {
       alert("City not found. ");
     });
+
+  fetch(forecastUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          displayCards(data, location);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("City not found. ")
+    });
 }
+
+function displayWeather(weather, location) {
+  if (weather.length === 0) {
+    alert("No data found. ")
+  };
+
+  console.log(location);
+
+  var liEl = $("<li class='li-el-results value='" + location + "'>" + location + "</li>");
+  prevEl.append(liEl);
+
+  cityArr.push(location);
+  console.log(cityArr);
+
+  let allInput = JSON.stringify(cityArr);
+  localStorage.setItem("city", allInput);
+  console.log(localStorage.getItem("city"));
+
+  let h2El = $("<h1>");
+  let imgEl = $("<img>");
+  let icon = weather.weather[0].icon;
+  console.log(icon);
+
+  let iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+  imgEl.attr("src", iconUrl);
+
+  h2El = $("<h2 class='header-result'>" + location + " " + currentDate + "</h2>");
+  currentWeather.append(h1El);
+  h2El.append(imgEl);
+
+  let temp = weather.main.temp;
+  let wind = weather.main.wind;
+  let humidity = weather.main.humidity;
+
+  currentWeather.append("<p class='temp-today'>Temp: " + temp + "°C</p>")
+  currentWeather.append("<p class='wind-today'>Wind Speed: " + wind + "MPH</p>")
+  currentWeather.append("<p class='temp-today'>Humidity: " + humidity + "%</p>")
+  currentWeather.removeClass("d-none")
+
+  let getLat = weather.coord.lat;
+  let getLon = weather.coord.lon;
+
+  let uvUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + getLat + "&lon=" + getLon + "&exclude=hourly&appid=" + API_KEY;
+
+  fetch(uvUrl)
+    .then(function (response) {
+      if (response.ok) {
+        console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+
+          renderUV(data)
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Error");
+    });
+
+  function renderUV(coordinates) {
+    if (coordinates.length === 0) {
+      alert("No UV Index found. ")
+    }
+
+    let uv = coordinates.current.uvi;
+
+    uvEl = $("<p class='uv-today'>UV Index: </p>");
+    uvSpan = $("<span class='uv-span'>" + uv + "</span>");
+    uvEl.append(uvSpan);
+    currentWeather.append(uvEl);
+
+    if (uv < 2) {
+      uvSpan.addClass("green");
+      uvSpan.append(" (Low UV)");
+    } else if (uv < 5) {
+      uvSpan.addClass("yellow");
+      uvSpan.append(" (Moderate UV)");
+
+    } else if (uv < 7) {
+      uvSpan.addClass("orange");
+      uvSpan.append(" (High UV)");
+
+    } else if (uv < 10) {
+      uvSpan.addClass("red");
+      uvSpan.append(" (Very High UV)");
+
+    } else if (uv > 10) {
+      uvSpan.addClass("dark-red");
+      uvSpan.append(" (Extreme UV)");
+    }
+  };
+};
+
+function displayCards(weather, location) {
+  let day = [0, 8, 16, 24, 32];
+
+  futureWeather.append("<section class='weather-forecast-item'></section>")
+  day.forEach(function (i) {
+    let createP = $("<p>")
+    createP.addClass("date");
+    futureWeather.append(createP);
+
+    let date = moment(weather.list[i].dt_txt).format("DD/MM/YYYY");
+
+    let futureTemp = weather.list[i].main.temp;
+    let futureWind = weather.list[i].wind.speed;
+    let futureHumidity = weather.list[i].main.humidity;
+
+    createP.append("<h2 class='header-result'>" + date + "</h2>");
+    createP.append("<img src='http://openweathermap.org/img/wn/" + weather.list[i].weather[0].icon + "@2x.png'</img>");
+    createP.append("<p class='temp'>Temp: " + futureTemp + " °C</p>");
+    createP.append("<p class='wind'>Wind Speed: " + futureWind + " MPH</p>");
+    createP.append("<p class='humidity'>Humidity: " + futureHumidity + " %</p>");
+    futureWeather.removeClass("d-none");
+
+  })
+}
+
+function handleList(event) {
+  event.preventDefault();
+
+  currentWeather.empty();
+  futureWeather.empty();
+
+  let location = $(event.target).text()
+  console.log(location);
+
+  if (location) {
+    getWeather(location);
+    $(event.target).remove();
+  };
+};
+
+prevEl.on("click", $(".li-el-results"), handeList);
+submitBtn.on("click", inputHandler);
